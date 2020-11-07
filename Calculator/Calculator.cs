66cf696149600fdab57b75f3a85fae74015f39be
janis-prusaks <janim.prusakam@gmail.com>
2020-11-07 18:12:12 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ievadi skaitli x: ");
+            double x;
+            
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Kļūda. Ievadi skaitli x: ");
+            }
+
+            Console.Write("Ievadi skaitli y: ");
+            double y;
+
+            while (!double.TryParse(Console.ReadLine(), out y))
+            {
+                Console.Write("Kļūda. Ievadi skaitli y: ");
+            }
+
+            Console.Write("Ievadi aritmētisko operāciju ( + - * / ): ");
+            string math = Console.ReadLine();
+
+            while (!math.Equals("+") && !math.Equals("-") && !math.Equals("*") && !math.Equals("/"))
+            {
+                Console.Write("Kļūda. Ievadi aritmētisko operāciju ( + - * / ): ");
+                math = Console.ReadLine();
+            }
+
+
+            switch(math)
+            {
+                case "+":
+                    Console.WriteLine($"{x} + {y} = {Utils.Add(x, y)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{x} - {y} = {Utils.Subtract(x, y)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{x} x {y} = {Utils.Multiply(x, y)}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{x} / {y} = {Utils.Divide(x, y)}");
+                    break;
+                default:
+                    Console.WriteLine("Nekorekti ievadīta aritmētiskā operācija");
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Utils
+    {
+        public static double Add (double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtract (double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply (double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide (double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
